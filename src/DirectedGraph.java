ade01c4a2f13e6b91d72d9ef458e5bc060186aae
DecibelM <42909365+DecibelM@users.noreply.github.com>
2018-12-17 21:04:02 +0100

    Working version of Dijkstra

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -1,5 +1,6 @@
 
 import static java.lang.System.out;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -37,16 +38,24 @@ public class DirectedGraph<E extends Edge> {
                     return current.getPath().iterator();
                 } else {
                     visited[current.getTo()] = true;
+
                     for (int i = 0; i < edgeList[current.getTo()].size(); i++) {
                         E edge = edgeList[current.getTo()].get(i); //x.get(i) är en busedge;
                         double cost = current.getCost() + edge.getWeight();
-						List<E> path = new ArrayList();
-						if(current.getPath() != null) {
-                            path = current.getPath();
-                        }
-						path.add(edge); //Vad ska läggas till? HUr när och varför?
 
-						CompDijkstraPath cdp = new CompDijkstraPath(edge.to, cost, path); //Är det edge.to?
+
+                        List<E> path = new ArrayList();
+                        if (current.getPath() != null) {
+                            List<E>  tmp = current.getPath();
+                            for(E e : tmp){
+                                path.add(e);
+                            }
+                            //path = current.getPath();
+                        }
+                        path.add(edge); //Vad ska läggas till? HUr när och varför?
+
+
+                        CompDijkstraPath cdp = new CompDijkstraPath(edge.to, cost, path); //Är det edge.to?
                         pq.add(cdp);
                     }
                 }
