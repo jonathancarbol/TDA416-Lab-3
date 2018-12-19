7092c1b4c5b715537eb94c807f1fdd94a5ee3700
DecibelM <42909365+DecibelM@users.noreply.github.com>
2018-12-19 10:47:56 +0100

    Added JavaDoc

--- src/CompDijkstraPath.java
+++ src/CompDijkstraPath.java
@@ -1,25 +1,38 @@
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 
+/**
+ * CompDijkstraPath is a class for holding elements for
+ * computing shortest path with Dijkstras algorithm. It holds a
+ * method for comparing elements.
+ * @param <E> E extends Edge
+ */
 public class CompDijkstraPath<E extends Edge> implements Comparable<CompDijkstraPath<E>> {
 
     private final int to;
     private double cost;
     private List<E> path;
 
+    /**
+     * @param to the number of the node reached
+     * @param cost the cost of the path from the start node
+     * @param path the previous path from the start node
+     */
     public CompDijkstraPath(int to, double cost, List<E> path) {
         this.to = to;
         this.cost = cost;
         this.path = path;
     }
 
-
+    /**
+     * The compareTo method compares the objects, looking at
+     * the cost and length of the path.
+     * @param e CompDijkstraPath
+     * @return 1 or -1 depending on what object is the largest.
+     */
     @Override
     public int compareTo(CompDijkstraPath e) {
-        if (this.cost <= e.getCost()){
-            if(this.getPath().size()<= e.getPath().size()) {
+        if (this.cost <= e.getCost()) {
+            if (this.getPath().size() <= e.getPath().size()) {
                 return -1;
             }
         }
@@ -29,11 +42,9 @@ public class CompDijkstraPath<E extends Edge> implements Comparable<CompDijkstra
     public int getTo() {
         return to;
     }
-
     public double getCost() {
         return cost;
     }
-
     public List<E> getPath() {
         return path;
     }
--- src/CompKruskalEdge.java
+++ src/CompKruskalEdge.java
@@ -1,3 +1,9 @@
+/**
+ * Class for holding elements for computing minimum spanning tree,
+ * using Kruskals algorithm. It has a compareTo method for comparing
+ * elements.
+ * @param <E> e extends Edge
+ */
 public class CompKruskalEdge<E extends Edge> implements Comparable<CompKruskalEdge<E>>  {
 
     private int from;
@@ -5,7 +11,10 @@ public class CompKruskalEdge<E extends Edge> implements Comparable<CompKruskalEd
     private double weight;
     private E bedge;
 
-
+    /**
+     * Constructor for CompKruskalEdge.
+     * @param edge An edge between to nodes.
+     */
     public CompKruskalEdge(E edge){
         this.from = edge.from;
         this.to = edge.to;
@@ -13,21 +22,18 @@ public class CompKruskalEdge<E extends Edge> implements Comparable<CompKruskalEd
         this.bedge = edge;
     }
 
-
+    /**
+     * The compareTo method compares the objects, looking at
+     * the weight.
+     * @param cke CompKruskalEdge
+     * @return -1 or 1 depending on what object is largest.
+     */
     @Override
     public int compareTo(CompKruskalEdge<E> cke) {
         if(this.getWeight() <= cke.getWeight()){
             return -1;
         }
         return 1;
-    }
-
-    public int getFrom() {
-        return from;
-    }
-
-    public int getTo() {
-        return to;
     }
 
     public double getWeight() {
