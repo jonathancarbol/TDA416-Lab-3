ed183cbc661c436694bb3c330d2abcf4475b1f8c
jonathancarbol <43402855+jonathancarbol@users.noreply.github.com>
2018-12-19 10:47:24 +0100

    Added java doc for DirectedGraph class, as well as commented code.

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -8,9 +8,10 @@ import java.util.PriorityQueue;
 
 
 /**
- * Directed graph - En klass för bestämning av den kortarste vägen melland två hållplatser och det minsta uppspännande
- * träd.
- * @param <E> som är en generic som extendar Edge.
+ * Directed graph - A class used to determine the shortest path between two stops as well as the minimum spanning tree.
+ * En klass för bestämning av den kortarste vägen melland två hållplatser och det minsta uppspännande träd.
+ *
+ * @param <E> which is a generic that extends Edge.
  *
  * @author Jonathan Carbol & Maria Fornmark
  * @version 1.0
@@ -22,9 +23,12 @@ public class DirectedGraph<E extends Edge> {
 
 
     /**
+     * The constructor for the klass with number of nodes as input. It creates an empty list of the same size as
+     * number of nodes.
      * Konstruktorn för klassen som har antal hållplatser som input. Den skapar samtidigt en tom list av samma storlek
      * som antal hållplatser.
-     * @param noOfNodes - antal hållplatser eller noder i inputen.
+     *
+     * @param noOfNodes - Number of nodes or buss stops.
      */
     public DirectedGraph(int noOfNodes) {
         this.noOfNodes = noOfNodes;
@@ -32,9 +36,12 @@ public class DirectedGraph<E extends Edge> {
     }
 
     /**
+     * Method used to add the edges to the DirectedGraph. The edges can beused to calculate the shortest path and the
+     * MST.
      * Metod för tillägning av bågar till DirectedGraph. Tar in alla bågar som används för bestämning av kortaste vägen
      * och MST.
-     * @param e - bågen som ska läggas till.
+     *
+     * @param e - The edge which is to be added.
      */
     public void addEdge(E e) {
         if (edgeList[e.from] == null) {
@@ -45,30 +52,32 @@ public class DirectedGraph<E extends Edge> {
     }
 
     /**
+     * Method used to determine the shortest path between two stops. It uses Dujkstras algorithm to determine this.
      * Metod för bestämning av kortaste vägen mellan två hållplatser. Använder Dijkstras algoritm för att bestämma detta.
-     * @param from - Bågens starthållplats.
-     * @param to - Bågens ändhållplats.
-     * @return En iterator med kortaste vägen.
+     *
+     * @param from - The edges starting point.
+     * @param to - The edges end point.
+     * @return An iterator with the shortest path.
      */
     public Iterator<E> shortestPath(int from, int to) {
-        PriorityQueue<CompDijkstraPath> pq = new PriorityQueue<>();
-        boolean[] visited = new boolean[noOfNodes];
+        PriorityQueue<CompDijkstraPath> pq = new PriorityQueue<>(); // A priority queue which is sorted according to CompDijkstraPath.
+        boolean[] visited = new boolean[noOfNodes]; // A list of all the nodes with a boolean to determine if the path for it already been calculated.
 
-        pq.add(new CompDijkstraPath(from, 0, null));
+        pq.add(new CompDijkstraPath(from, 0, null)); // Starting point added to the priority queue.
         while (!pq.isEmpty()) {
             CompDijkstraPath current = pq.remove();
-            if (visited[current.getTo()] == false) {
+            if (visited[current.getTo()] == false) { // Check to see if the nodes has already been calculated.
                 if (current.getTo() == to) {
-                    return current.getPath().iterator();
+                    return current.getPath().iterator(); // Returns the path is when it has been determined.
                 } else {
                     visited[current.getTo()] = true;
 
-                    for (int i = 0; i < edgeList[current.getTo()].size(); i++) {
+                    for (int i = 0; i < edgeList[current.getTo()].size(); i++) { // Loops through all nodes connected to the current node.
                         E edge = edgeList[current.getTo()].get(i);
-                        double cost = current.getCost() + edge.getWeight();
+                        double cost = current.getCost() + edge.getWeight(); // Calculates the new cost of the edge.
 
                         List<E> path = new ArrayList();
-                        if (current.getPath() != null) {
+                        if (current.getPath() != null) { // Changes the path of the edges.
                             List<E>  tmp = current.getPath();
                             for(E e : tmp){
                                 path.add(e);
@@ -76,8 +85,8 @@ public class DirectedGraph<E extends Edge> {
                         }
                         path.add(edge);
 
-                        CompDijkstraPath cdp = new CompDijkstraPath(edge.to, cost, path);
-                        pq.add(cdp);
+                        CompDijkstraPath cdp = new CompDijkstraPath(edge.to, cost, path); // Creates a new CompDijkstraPath.
+                        pq.add(cdp); // Adds the CDP to the priority queue.
                     }
                 }
             }
@@ -86,17 +95,19 @@ public class DirectedGraph<E extends Edge> {
     }
 
     /**
+     * A method used to determine the minimum spanning tree for all buss stops. It uses Kruskals algorithm with lists.
      * En metod för bestämning av minsta uppspända trädet för alla hållplatser. Använder Kruskals algoritm med listor.
-     * @return En lista med alla noder i MST:n.
+     *
+     * @return A list with all the nodes in the MST.
      */
     public Iterator<E> minimumSpanningTree() {
-        List[] cc = new List[noOfNodes];
-        for (int i = 0; i < cc.length; i++){
+        List[] cc = new List[noOfNodes]; // Creates an array with number of nodes as size.
+        for (int i = 0; i < cc.length; i++){ // Creates an empty list for every node.
             cc[i] = new ArrayList<BusEdge>();
         }
 
-        PriorityQueue<CompKruskalEdge> pq = new PriorityQueue<>();
-        for (int i = 0; i < edgeList.length; i++){
+        PriorityQueue<CompKruskalEdge> pq = new PriorityQueue<>(); // New priority queue sorted according to CompKruskalEdge.
+        for (int i = 0; i < edgeList.length; i++){ // Adds all edges to the priority queue.
             for (int j = 0; j < edgeList[i].size(); j++){
                 E be = edgeList[i].get(j);
                 pq.add(new CompKruskalEdge(be));
@@ -106,31 +117,31 @@ public class DirectedGraph<E extends Edge> {
         int large;
         int small;
         while (!pq.isEmpty() && cc.length > 0){
-            Edge e = pq.remove().getBedge();
+            Edge e = pq.remove().getBedge(); // Remove first item from priority queue.
 
-            if ( cc[e.from] != cc[e.to]){
-                if (cc[e.from].size() >= cc[e.to].size()){
+            if ( cc[e.from] != cc[e.to]){ // Check to see that from and to does not refer to the same list.
+                if (cc[e.from].size() >= cc[e.to].size()){ // Check to see which of the lists are smallest.
                     large = e.from;
                     small = e.to;
                 }else {
                     large = e.to;
                     small = e.from;
                 }
-                if(cc[small].size() > 0) {
+                if(cc[small].size() > 0) { // Adds the smaller list to the larger list.
                     for (int i = 0; i < cc[small].size(); i++) {
                         cc[large].add(cc[small].get(i));
                     }
                 }
-                cc[large].add(e);
+                cc[large].add(e); // Adds the item removed from the priority queue.
 
-                for (int i = 0; i < cc[large].size(); i++){
+                for (int i = 0; i < cc[large].size(); i++){ // Goes through the new larger lists and repoint all the other lists to that list.
                     BusEdge b = (BusEdge) cc[large].get(i);
                     cc[b.from] = cc[large];
                     cc[b.to] = cc[large];
                 }
             }
         }
-        return cc[0].iterator();
+        return cc[0].iterator(); // Returns the list with all nodes in a MST.
     }
 }
   
